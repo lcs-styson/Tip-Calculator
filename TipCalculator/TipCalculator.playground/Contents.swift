6f22ee949a7bf2a970c6e9c865fc15681d1fc968
Sebastian Tyson <styson@lcs.on.ca>
2019-10-17 15:58:09 -0400

    Set everything up

--- TipCalculator/TipCalculator.playground/Contents.swift
+++ TipCalculator/TipCalculator.playground/Contents.swift
@@ -7,10 +7,20 @@ let splitBetweenHowManyPeople = UITextField()
 
 // Simulate userinput
 billAmount.text = "100"
-tipPercentage.text = "15"
+tipPercentage.text = ".15"
 splitBetweenHowManyPeople.text = "2"
 
 // Get actual strungs from each text field
+// (Force unwrapping of the optional strings contined in each text field)
+let billAmountAsString = billAmount.text!
+let tipPercentageAsString = tipPercentage.text!
+let splitBetweenHowManyPeopleAsString = splitBetweenHowManyPeople.text!
 
+// Convert strings to double data types
 
-//
+let billAmountAsDouble = Double(billAmountAsString)!
+let tipPercentageAsDouble = Double(tipPercentageAsString)!
+let splitBetweenHowManyPeopleAsDouble = Double(splitBetweenHowManyPeopleAsString)!
+
+// Calculate the tip
+let tipAmountInDollars = billAmountAsDouble * tipPercentageAsDouble
--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var submittedPeopleCount: UITextField!
+    @IBOutlet weak var submittedBillAmount: UITextField!
+    @IBOutlet weak var submittedTip: UITextField!
     override func viewDidLoad() {
+    
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        // Text fields
+        let billAmount = submittedBillAmount!
+        let tipPercentage = submittedTip!
+        let peopleCount = submittedPeopleCount!
+        
+        
+        // Get actual strungs from each text field
+        // (Force unwrapping of the optional strings contined in each text field)
+        let billAmountAsString = billAmount.text!
+        let tipPercentageAsString = tipPercentage.text!
+        let peopleCountAsString = peopleCount.text!
+        
+        // Convert strings to double data types
+        
+        let billAmountAsDouble = Double(billAmountAsString)!
+        let tipPercentageAsDouble = Double(tipPercentageAsString)!
+        let peopleCountAsDouble = Double(peopleCountAsString)!
+        
+        // Calculate the tip
+        let tipAmountInDollars = billAmountAsDouble * tipPercentageAsDouble
     }
 
 
